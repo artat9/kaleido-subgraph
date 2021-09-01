0906fd6f5ceb2bb741dcdb5e75a4d0fe0f62521f
atatur9 <momodarou2015@gmail.com>
2021-09-01 21:36:57 +0900

    define handleBid

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,7 +1,7 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
-import { NewPost } from "./generated/AdManager/AdManager";
-import { PostContent } from "./generated/schema";
+import { Bid, NewPost } from "./generated/AdManager/AdManager";
+import { Bidder, PostContent } from "./generated/schema";
 
 export function handleNewPost(event: NewPost): void {
   let post = new PostContent(toId(event.params.postId));
@@ -10,6 +10,16 @@ export function handleNewPost(event: NewPost): void {
   post.fromTimestamp = event.params.fromTimestamp.toI32();
   post.toTimestamp = event.params.toTimestamp.toI32();
   post.save();
+}
+
+export function handleBid(event: Bid): void {
+  let bidder = new Bidder(toId(event.params.bidId));
+  bidder.post = toId(event.params.postId);
+  bidder.metadata = event.params.metadata;
+  bidder.originalLink = event.params.originalLink;
+  bidder.price = event.params.price;
+  bidder.status = "LISTED";
+  bidder.save();
 }
 
 let toId = (postId: BigInt): string => {
