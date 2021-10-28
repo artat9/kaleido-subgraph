c01271f2d60bd75d3d84b9d12e70fc1c6bf13ab8
atatur9 <momodarou2015@gmail.com>
2021-10-28 15:05:52 +0900

    modify events

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/bridges-inc/kaleido-core.git
 cd kaleido-core
 git fetch
-git pull origin develop
+git pull origin release/v1.0
 yarn && npx hardhat compile
 cd ..
 yarn manifest:dev
